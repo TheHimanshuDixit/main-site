d481614549e16213d96269b7d17e7f67b5b23234
TheHimanshuDixit <himanshuharshdixit@gmail.com>
2023-10-04 23:15:34 +0530

    some changes head update

--- src/components/Home/childs/Mainpage.jsx
+++ src/components/Home/childs/Mainpage.jsx
@@ -26,11 +26,6 @@ const Mainpage = () => {
       <div className="text-3xl font-[#141718] mt-2">
         <h2>Jaypee Institute Of Information Technology 62</h2>
       </div>
-
-      <div className='pt-20 me-auto'>
-        <Animation />
-      </div>
-
       <div className="text-xl font-[#141718] mt-2">
         <h4>Jaypee Institute Of Information Technology 62</h4>
       </div>
@@ -43,6 +38,9 @@ const Mainpage = () => {
         <h4>At GDSC JIIT 62, we aspire to ignite the flames of innovation within young developers, guiding them to unearth </h4>
         <h4 className='mt-2'>their hidden talents and embark on a transformative journey of <span className='border-2 rounded-3xl py-1 px-2 bg-blue-300 border-blue-500'>self-discovery</span></h4>
         </div>
+        <div className='pt-20 me-auto'>
+        <Animation />
+      </div>
     </div>
   );
 };
